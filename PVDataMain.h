30395bb385596fe8e52074543e85672b0a9ebc31
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2020-11-17 23:52:19 +0800

    Create PVDataMain.h
    Initial Commit

---
+++ PVDataMain.h
@@ -0,0 +1,14 @@
+#ifndef PVDATAMAIN_H
+#define PVDATAMAIN_H
+
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
+
+class PVDataMain : public Gtk::Window
+{
+    private:
+    protected:
+    public:
+}
+
+#endif // PVDATAMAIN_H
