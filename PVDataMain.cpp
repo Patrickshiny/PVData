c18ed7967cd55f7b1cdf480923bdd888ed052d2c
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2020-11-20 23:38:05 +0800

    Update PVDataMain.cpp

--- PVDataMain.cpp
+++ PVDataMain.cpp
@@ -6,11 +6,13 @@ int main (int argc, char *argv[])
   PVDataMain* pCurrentPVDataMain = new PVDataMain();
   if (pCurrentPVDataMain != NULL)
   {
+      // app->
       return app->run(*pCurrentPVDataMain);
   }
 }
 
 PVDataMain::PVDataMain()
+: m_button("WindowCtrlButton")
 {
   set_border_width(1920);
   set_border_height(1080);
@@ -23,4 +25,5 @@ PVDataMain::~PVDataMain()
 
 void PVDataMain::onTopButtonClick()
 {
+
 }
